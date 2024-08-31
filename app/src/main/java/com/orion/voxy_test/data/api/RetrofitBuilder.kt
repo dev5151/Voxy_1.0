9ef73ae41465d908105a621dd91318a3a6f19f39
dev5151 <iamdev5151@gmail.com>
2024-08-31 11:29:10 +0530

    add: reformat code

--- app/src/main/java/com/orion/voxy_test/data/api/RetrofitBuilder.kt
+++ app/src/main/java/com/orion/voxy_test/data/api/RetrofitBuilder.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitBuilder {
-    private const val BASE_URL = "https://chatbot-response-2-8l5560u1.an.gateway.dev"
+    private const val BASE_URL = "https://chatbot-response-2-8l5560u1.an.gateway.dev/"
 
     private fun getRetrofit(): Retrofit {
         return Retrofit.Builder()
